56ff099dc9d9f98aa4a6a5d55151a23f6e72f216
David Finol <davidmfinol@gmail.com>
2025-08-20 14:33:37 -0500

    Fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.finoldigital.ygolp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.2.1"
 
     }
 
